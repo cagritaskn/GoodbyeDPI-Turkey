bdd7a6e4d5d913159ce4c4af16f74de5c2c3b73e
Brine28 <asiyee994@gmail.com>
2026-05-20 21:14:39 +0300

    Add blackwhitelist_clear function
    Add function to clear blacklist and free resources

--- src/blackwhitelist.h
+++ src/blackwhitelist.h
@@ -3,3 +3,6 @@ int blackwhitelist_load_list(const char *filename);
 
 // Check hostname against list, returns match status
 int blackwhitelist_check_hostname(const char *host_addr, size_t host_len);
+
+// Clear the loaded blacklist and free all resources
+void blackwhitelist_clear(void);
